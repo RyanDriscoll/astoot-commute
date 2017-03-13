c5915c7c2c2efd228d17216b0816014b508771af
Ryan Driscoll <driscollrp@gmail.com>
2017-03-13 11:05:18 -0500

    plugging in heroku db

--- db/db.js
+++ db/db.js
@@ -1,12 +1,12 @@
 'use strict';
 const chalk = require('chalk');
 const Sequelize = require('sequelize');
-const DATABASE_URI = 'postgres://localhost:5432/astoot';
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/astoot';
 
 console.log(chalk.yellow('Opening connection to PostgreSQL'));
 
 // create the database instance
-module.exports = new Sequelize(DATABASE_URI, {
+module.exports = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: true // lets Sequelize know we can use pg-native for ~30% more speed
 });
